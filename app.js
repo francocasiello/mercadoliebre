61ad4e1985cfeb6064f68def68a27a58492d8ba5
francocasiello <francocasiello@gmail.com>
2021-10-11 22:59:07 -0300

    second commit

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ app.use(express.static(publicPath));
 
 app.use(express.urlencoded());
 
-app.listen(3000, () => {
+//app.listen(3000, () => {
+//    console.log("Servidor corriendo en el puerto 3000");
+//});
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Servidor corriendo en el puerto 3000");
-});
+})
 
 
 app.get("/", (req, res) => {
