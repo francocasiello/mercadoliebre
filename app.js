c9b9da79039d0273c50a9ae0f115feb977c5864a
francocasiello <francocasiello@gmail.com>
2021-10-13 21:09:11 -0300

    probando subir a heroku

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ app.use(express.static(publicPath));
 
 app.use(express.urlencoded());
 
-//app.listen(3000, () => {
-//    console.log("Servidor corriendo en el puerto 3000");
-//});
-
 app.listen(process.env.PORT || 3000, function () {
     console.log("Servidor corriendo en el puerto 3000");
 })
